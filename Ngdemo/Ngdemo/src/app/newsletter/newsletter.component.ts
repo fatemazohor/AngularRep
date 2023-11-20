43bbae44850ddda703277ae6121b92dfa2356c88
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-11-20 13:09:50 +0600

    add20-11-23

--- Ngdemo/Ngdemo/src/app/newsletter/newsletter.component.ts
+++ Ngdemo/Ngdemo/src/app/newsletter/newsletter.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faTwitter,faInstagram,faFacebook,faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-newsletter',
@@ -9,5 +12,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './newsletter.component.css'
 })
 export class NewsletterComponent {
-
+  faLinkdin=faLinkedin
+  faFacebook=faFacebook
+  faTwitter=faTwitter
+  faYoutube=faYoutube
+  faInstagram=faInstagram
 }
