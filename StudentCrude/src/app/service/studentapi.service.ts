d5b4559f7b3db1172d983aa0fc27380d28b6e684
Fatema Tuz Zohora <fatematuzzohora289@gmail.com>
2023-11-22 23:50:19 +0600

    update

--- StudentCrude/src/app/service/studentapi.service.ts
+++ StudentCrude/src/app/service/studentapi.service.ts
@@ -6,11 +6,27 @@ import { map } from 'rxjs';
   providedIn: 'root'
 })
 export class StudentapiService {
-  baserUrl:string='http://localhost:3000/posts';
+  baserUrl:string='http://localhost:3000/posts/';
   constructor(private http:HttpClient) { }
 
   saveStudent(data:any){
-    return this.http.put<any>(this.baserUrl,data)
+    return this.http.post<any>(this.baserUrl , data)
     .pipe(map(res=>{return res;}))
   }
+
+  getAll(){
+    return this.http.get<any>(this.baserUrl)
+    .pipe(map(res=>{return res}))
+  }
+
+  deleteStudent(id:number){
+    return this.http.delete<any>(this.baserUrl + id)
+    .pipe(map(res => {return res}))
+  }
+
+  editStudent(id:number, data:any){
+    return this.http.put<any>(this.baserUrl + id, data)
+    .pipe(map(res => {return res}))
+  }
+
 }
--- StudentCrude/src/app/students/students.component.ts
+++ StudentCrude/src/app/students/students.component.ts
@@ -26,6 +26,7 @@ export class StudentsComponent implements OnInit{
       subject:[''],
       roll:['']
     })
+    this.getAllstudetn();
     throw new Error('Method not implemented.');
   }
 
@@ -38,13 +39,57 @@ export class StudentsComponent implements OnInit{
     this.studentModel.subject=this.formValue.value.subject;
     this.studentModel.roll=this.formValue.value.roll;
     this.api.saveStudent(this.studentModel)
-    .subscribe({
-      next(res) {
+    .subscribe(
+      res=>{
         console.log(res)
+        alert("Data saved.")
+        this.formValue.reset()
+        this.getAllstudetn();
       },
-      error(err) {
-        console.log(err)
+      err=>{
+        alert("Data not saved.")
       }
+    )
+  }
+
+  getAllstudetn(){
+    this.api.getAll().subscribe(
+      {next:res=>{this.studentData=res}}
+    )
+  }
+  deleteStu(row:any){
+    this.api.deleteStudent(row.id)
+    .subscribe({next:res => {
+      console.log(res)
+      alert("Data is deleted.")
+      this.formValue.reset()
+      this.getAllstudetn()
+    
+    },
+    error:err => alert("Data not deleted")})
+  }
+
+  editbyId(row : any){
+    this.studentModel.id=row.id;
+    this.formValue.controls['name'].setValue(row.name)
+    this.formValue.controls['subject'].setValue(row.subject)
+    this.formValue.controls['roll'].setValue(row.roll)
+  }
+
+  editStu(){
+    this.studentModel.name = this.formValue.value.name;
+    this.studentModel.subject = this.formValue.value.subject;
+    this.studentModel.roll = this.formValue.value.roll;
+
+    this.api.editStudent(this.studentModel.id, this.studentModel)
+    .subscribe({next:res =>{
+      console.log(res)
+      alert("Data edited")
+      this.formValue.reset()
+      this.getAllstudetn()
+    },
+    error: err => {alert("Data not edited.")} 
     })
   }
+
 }
